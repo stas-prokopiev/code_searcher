7291d8130163686373345a91aacc122f205cc57e
stanislav <stas.prokopiev@gmail.com>
2020-01-07 03:44:42 +0300

    0.0.12

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,16 +1,30 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-from code_searcher.skeleton import fib
+# import pytest
+import code_searcher
+from code_searcher.code_searcher_class import code_searcher_class
+import os
 
-__author__ = "stanislav"
-__copyright__ = "stanislav"
+__author__ = "stanislav prokopyev"
+__copyright__ = "stanislav prokopyev"
 __license__ = "mit"
 
 
-def test_fib():
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(7) == 13
-    with pytest.raises(AssertionError):
-        fib(-10)
+class Test_code_searcher(unittest.TestCase):
+
+    def test_code_searching():
+        """"""
+        str_code_searcher_file = os.path.abspath(code_searcher.__file__)
+        str_folder_with_files = os.path.dirname(str_code_searcher_file)
+        list_str_folders_where_to_look = [str_folder_with_files]
+        code_searcher_obj = code_searcher_class(
+            list_str_folders_where_to_look,
+            list_str_file_extensions=[".py"],
+        )
+        int_occurences = \
+            code_searcher_obj.search_all_occurrences_of_the_code_in_the_library(
+                "class code_searcher_class"
+            )
+        assert int_occurences == 1, "ERROR"
+
+
